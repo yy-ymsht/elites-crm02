373651335d47ea612907873f887d3926b2712000
yuya yamashtia <yama.98904@gmail.com>
2016-11-13 08:03:11 +0000

    01_実習_customerのCRUD周りを開発/show周りを作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,9 +3,13 @@ class CustomersController < ApplicationController
   end
 
   def new
+    @customer = Customer.new
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to @customer
   end
 
   def edit
@@ -15,8 +19,21 @@ class CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def destroy
   end
-end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email
+      )
+    
+  end
+
+end 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ActiveRecord::Base
+	def full_name
+	# return family_name + given_nameの略
+		family_name + given_name
+	end
 end
