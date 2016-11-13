59d857967ddcba1e74f50c57647241809a840151
yuya yamashtia <yama.98904@gmail.com>
2016-11-13 22:37:34 +0000

    01_実習_customerのCRUD周りを開発/バリデーションを実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,8 +8,11 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.new(customer_params)
-    @customer.save
-    redirect_to @customer
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
   end
 
   def edit
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,12 @@
 class Customer < ActiveRecord::Base
+	validates :family_name,
+	  presence: true, length: { maximum: 20 }
+	validates :given_name,
+	  presence: true, length: { maximum: 20 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email,
+	  presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+	  
 	def full_name
 	# return family_name + given_nameの略
 		family_name + given_name
