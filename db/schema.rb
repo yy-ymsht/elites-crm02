e3ad59f70da0e4dfae701bad383da5a8f1a416fa
yuya yamashtia <yama.98904@gmail.com>
2016-11-27 03:05:52 +0000

    06_実習_userにカラム追加する

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125223804) do
+ActiveRecord::Schema.define(version: 20161126080507) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20161125223804) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "family_name"
+    t.string   "given_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
